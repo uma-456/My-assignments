569f6074844ec017097b4a4fbdba70a1e7da2a39
uma-456 <umautthandan@gmail.com>
2026-07-18 20:40:02 +0530

    Add files via upload
    day 1 assignment

---
+++ datatype1.js
@@ -0,0 +1,11 @@
+let firstName = 'uma'
+console.log(firstName)
+let companyName ='ltm mindtree'
+console.log(companyName)
+let mobileNumber =987456321
+console.log(mobileNumber)
+let isAutomation =true
+console.log(isAutomation)
+let hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof firstName,typeof companyName,typeof mobileNumber,typeof isAutomation,typeof hasPlaywright)
