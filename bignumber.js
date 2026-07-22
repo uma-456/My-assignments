3052329e8b68f884033070c3cb3b199824dd29e0
uma-456 <umautthandan@gmail.com>
2026-07-22 11:20:19 +0530

    Add files via upload
    day2 bignumber

---
+++ bignumber.js
@@ -0,0 +1,14 @@
+function bigNumber(a)
+{
+    if(a<0){
+        console.log("negative")
+}
+    else if(a>0){
+console.log("positive")}
+    else
+    {
+        console.log(0)}
+}
+bigNumber(6)
+
+    
